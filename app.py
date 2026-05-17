063fc42347cd9a1cbf80e1a7cae2b414233f64d1
agendakegiatandinkes-design <agendakegiatandinkes@gmail.com>
2026-05-17 18:55:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def load_data():
 # =========================
 # LOAD DATAFRAME EXECUTION
 # =========================
-if GOOGLE_SHEET_URL == "SALIN_LINK_GOOGLE_SHEETS_ANDA_DI_SINI":
+if GOOGLE_SHEET_URL =https://docs.google.com/spreadsheets/d/1sak_dmVop0yWI9tOCtJ_rfqcapUI3CekXGduUHE7sMo/edit?usp=sharing
     st.info("👋 Selamat Datang! Silakan masukkan link Google Sheets Anda pada kode app.py terlebih dahulu.")
     st.stop()
 
